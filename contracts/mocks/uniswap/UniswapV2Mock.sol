ca2587dcf899bfb00f2219e97ceaef38395c8d08
nxaaya <nxaaya@gmail.com>
2023-08-14 14:11:25 +0300

    test: uniswap v2

--- contracts/mocks/uniswap/UniswapV2Mock.sol
+++ contracts/mocks/uniswap/UniswapV2Mock.sol
@@ -3,3 +3,24 @@ pragma solidity ^0.8.15;
 
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+
+contract UniswapV2Mock {
+    uint256 rate;
+
+    function setRate(uint256 _rate) external {
+      rate = _rate;
+    }
+
+    function swapExactTokensForTokens(
+        uint256 amountIn,
+        uint256 amountOutMin,
+        address[] calldata path,
+        address to,
+        uint256 deadline
+    ) external returns (uint256[] memory amounts) {
+         address tokenB = path[1];
+
+         IERC20(tokenB).transfer(to, amountIn*rate / 10**18);
+    }
+}
