c7e3473928a66572d358ebf302489c2a6883e9cb
nxaaya <nxaaya@gmail.com>
2023-08-14 13:41:35 +0300

    test: uniswap v3 mock

---
+++ contracts/mocks/uniswap/UniswapV3Mock.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.8.15;
+
+import "../../interfaces/IUniV3.sol";
+import "./v3/BytesLib.sol";
+import "./v3/Path.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract UniswapV3Mock is IUniV3 {
+      using Path for bytes;
+
+      uint256 rate;
+
+      function exactInput(
+        ExactInputParams calldata params
+    ) override external payable returns (uint256 amountOut) {
+      (address tokenA, address tokenB, uint256 fee) = params.path.decodeFirstPool();
+
+      IERC20(tokenB).transfer(params.recipient, params.amountIn*rate / 10^18);
+    }
+
+    function setRate(uint256 _rate) external {
+      rate = _rate;
+    }
+}
